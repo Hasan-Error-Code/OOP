2350f5bd147243ccce8e5396a94fed31c53c5c9c
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-26 22:48:08 +0600

    Object

---
+++ first.py
@@ -0,0 +1,9 @@
+"""Class object Constructor attributes methods()"""
+class Student:
+    def __init__(self):
+        print(self)
+        print("a student object created")
+
+# variable = class_name
+s1 = Student()
+# print(s1)
