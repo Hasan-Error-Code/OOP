63a07a685a6cc193dbd77ba5aecfd8e058349690
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-28 12:49:15 +0600

    instance variable

---
+++ House.py
@@ -0,0 +1,14 @@
+class House:
+    def __init__(self):
+        self.window = 4
+        self.door = 2
+
+    def view(self):
+        print("Window:", self.window, "Door:", self.door)
+h1 = House()
+h2 = House()
+h1.door = 3
+h2.window = 6
+
+print(h1.view())
+print(h2.view())
