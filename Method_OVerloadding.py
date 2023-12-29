0c81c19b255587930df748131592a45cff81e6fa
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-29 11:51:34 +0600

    method overloading done

--- Method_OVerloadding.py
+++ Method_OVerloadding.py
@@ -1,8 +1,12 @@
 class Calculator:
     def product(self, num1, num2 = None, num3 = None):
         if num1 != None and num2 != None and num3 != None:
-            print(num1 * num2 * num3) 
+            print("num1 * num2 * num3:",num1 * num2 * num3) 
+        else:
+            print("num1 * num2:",num1 * num2)
 
 
 c1 = Calculator()
 c1.product(4, 6, 7)
+c2 = Calculator()
+c2.product(2, 8)
