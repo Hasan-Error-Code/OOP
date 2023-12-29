1565a1511ceed22e64dedf57b526fb17519a6f11
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-29 09:34:19 +0600

    Pass by value

---
+++ Pass_By_value.py
@@ -0,0 +1,19 @@
+class Cat:
+    def __init__(self, color, action):
+        self.color = color
+        self.action = action
+    def view(self,num,clr):
+        num = num + 5
+        clr1 = clr
+        clr1[0] = "Green"
+        print("Method inside:", num)
+        print("Method inside:", clr1)
+
+
+
+colors = ["Black", "Red", "Yellow", "Blue"]
+c1 = Cat("White", "Jumping")
+x = 55
+c1.view(x, colors)
+print("Method inside:", x)
+print("Method inside:", colors)
