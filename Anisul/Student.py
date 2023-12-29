b299562ac3ecfd0b0c23af1e45657451c971a066
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-29 22:50:13 +0600

    Student

---
+++ Anisul/Student.py
@@ -0,0 +1,13 @@
+class Student:
+    roll = ""
+    gpa = ""
+
+Rohim = Student()
+Rohim.roll = 120
+Rohim.gpa = 3.5
+print(f"Rohim roll {Rohim.roll} and gpa {Rohim.gpa}")
+
+Karim = Student()
+Karim.roll = 121
+Karim.gpa = 3.54
+print(f"Karim roll {Karim.roll} and gpa {Karim.gpa}")
