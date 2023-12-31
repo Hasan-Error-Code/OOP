e18400d2e2306919c7578849e2e0f99583725bb4
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-31 11:10:42 +0600

    modefied

--- Anisul/Constractor.py
+++ Anisul/Constractor.py
@@ -4,9 +4,10 @@ class Student:
 
 def __init__(self, roll, gpa):
   self.roll = roll
-  self.gap = gap
+  self.gap = gpa
 def display(self):
   print(f"Roll: {self.roll}, GPA: {self.gpa}")
 
 rahim = Student(101, 3.75)
 rahim.display()
+
--- Anisul/Student.py
+++ Anisul/Student.py
@@ -10,4 +10,5 @@ print(f"Rohim roll {Rohim.roll} and gpa {Rohim.gpa}")
 Karim = Student()
 Karim.roll = 121
 Karim.gpa = 3.54
+
 print(f"Karim roll {Karim.roll} and gpa {Karim.gpa}")
